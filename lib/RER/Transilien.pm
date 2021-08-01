ae25b4531bd17b058d52ff4691e99f6a9a9da7a7
x0r <x0r+bitbucket@x0r.fr>
2021-08-01 16:09:44 +0200

    Cleanup: split up large functions
    Some pieces of code were written as one large function that could instead be
    split up into smaller pieces.

--- lib/RER/Transilien.pm
+++ lib/RER/Transilien.pm
@@ -16,12 +16,7 @@ use Dancer qw(:syntax config debug error);
 sub new {
     my %param = @_;
 
-    my @trains;
-
-    # Obtention des trigrammes correspondant aux gares recherchées
-    my $gare_from;
-
-    $gare_from = RER::Gares::find(code => $param{'from'})
+    my $gare_from = RER::Gares::find(code => $param{'from'})
         || die $param{'from'} . ": gare non trouvée\n";
 
     if (! defined $param{ds}) {
@@ -58,89 +53,133 @@ sub new {
         @data = @{$ds[1]->get_next_trains($gare_from)};
     }
 
-    for (my $i = 0; $i < scalar(@data); $i++) {
-        my $train = $data[$i];
-        next if ! defined $train;
-
-        my $today = ($train->real_time) ? $train->real_time->ymd('-') 
-            : ($train->due_time) ? $train->due_time->ymd('-')
-            : `date +'%Y-%m-%d'`;
-
-        my $terminus_name = ($train->terminus) ? $train->terminus->name : "?";
-
-        my $time = ($train->real_time) ? $train->real_time->time :
-            ($train->due_time) ? $train->due_time->time :
-            "--:--";
-        $time = substr $time, 0, 5;
-
-
-
-        my ($delay, $delay_str);
-        if ($train->real_time && $train->due_time) {
-            $delay = $train->real_time - $train->due_time;
-            $delay = $delay->in_units('minutes');
-
-            $delay_str = sprintf "%+d h %02d", 
-                        (int($delay / 60)),
-                        (abs($delay % 60))  if abs $delay >= 60;
-            $delay_str = sprintf "%+d min", $delay if abs $delay < 60;
-            $delay_str = "à l’heure" if $delay == 0;
-        }
-
-        my @arr_dessertes;
-        my $dessertes = "Desserte indisponible";
-        if ($train->stations) {
-            @arr_dessertes = map { $_ ? ($_->name || $_->uic) : "Gare non trouvée" } @{$train->stations};
-            $dessertes = join " \x{2022} ", @arr_dessertes;
-        }
-
-        # L'attribut "status" indique si le train est retardé, supprimé...
-        my $time_info;
-        my $trainclass;
-        for ($train->status) {
-            if ($_ eq 'N') { # rien = Normal
-                $time_info = $time;
-                $trainclass = 'train';
-            } elsif ($_ eq 'R') {
-                $time_info = 'Retardé'; 
-                $trainclass = 'train delayed';
-            } elsif ($_ eq 'S') {
-                $time_info = 'Supprimé';
-                $trainclass = 'train canceled';
-            } elsif ($_ eq 'P') {
-                $time_info = 'À l\'approche';
-                $trainclass = 'train';
-            } elsif ($_ eq 'Q') {
-                $time_info = 'À quai';
-                $trainclass = 'train';
-            } else {
-                $time_info = "$time (MENTION '" . ($train->status || 'undef') . "' INCONNUE)"; 
-                $trainclass = 'train';
-            };
-        }
-
-        push @trains, { 
-            mission => $train->code, 
-            numero  => $train->number,
-            time    => $time_info,
-            destination => $terminus_name,
-            dessertes   => $dessertes,
-            platform => $train->platform,
-            trainclass => $trainclass,
-            retard => $delay_str,
-            ligne => $train->line,
-        };
-    }
-
     return RER::Results->new(
         from => $gare_from,
-        trains => \@trains,
+        trains => [map { train_to_json($_) } @data],
         messages => \@messages
     );
 }
 
+=head2 format_time_info
 
+Renvoie l’heure temps réel ou, à défaut, l’heure théorique sous la forme d’une
+chaîne de caractères (HH:MM).
 
+Cependant, si le train est retardé, cette fonction renvoie « Retardé » à la
+place ; s’il est supprimé, la fonction renvoie « Supprimé ».
+
+Si le train n’a aucune information d’heure théorique ou temps réel, alors la
+fonction renvoie la chaîne « --:-- ».
+
+=cut
+
+sub format_time_info {
+    my ($train) = @_;
+
+    return 'Retardé' if $train->status eq 'R';
+    return 'Supprimé' if $train->status eq 'S';
+    return "À l'approche" if $train->status eq 'P';
+    return 'À quai' if $train->status eq 'Q';
+    return '--:--' unless defined ($train->due_time // $train->real_time);
+
+    return ($train->real_time // $train->due_time)->strftime('%H:%M');
+}
+
+=head2 format_delay
+
+Renvoie le retard par rapport à l’heure théorique sous la forme d’une chaîne
+de caractères, par exemple « +5 min » ou « +1 h 30 » ou encore « −1 min ».
+
+Si l’heure temps réel n’est pas connue ou si le train est retardé ou supprimé,
+alors cette fonction renvoie undef.
+
+=cut
+
+sub format_delay {
+    my ($train) = @_;
+
+    return undef unless ($train->real_time && $train->due_time);
+    return undef if ($train->status eq 'R' || $train->status eq 'S');
+
+    my $delay = ($train->real_time - $train->due_time)->in_units('minutes');
+
+    my $hours = int(abs($delay) / 60);
+    my $minutes = abs($delay) % 60;
+    my $sign = ($delay >= 0) ? '+' : "\N{MINUS SIGN}";
+
+    if ($delay == 0) {
+        return 'à l’heure';
+    }
+    elsif ($hours == 0) {
+        return sprintf("%s%d min", $sign, $minutes);
+    }
+    else {
+        return sprintf("%s%d h %02d", $sign, $hours, $minutes);
+    }
+}
+
+=head2 train_css_class
+
+Renvoie une liste de classes CSS à utiliser pour la présentation du train
+donné en paramètre.
+
+=cut
+
+sub train_css_class
+{
+    my ($train) = @_;
+
+    for ($train->status) {
+        return 'train delayed'  if $_ eq 'R';
+        return 'train canceled' if $_ eq 'S';
+        return 'train';
+        # N = Normal
+        # P = À l’approche
+        # Q = À quai
+    }
+}
+
+=head2 train_to_json
+
+Génère un hash, adéquat pour être transformé en JSON par la suite, pour un
+élément d’un résultat de recherche des prochains trains d’une gare.
+
+=cut
+
+sub train_to_json
+{
+    my ($train) = @_;
+
+    return undef unless defined $train;
+
+    my $terminus_name = ($train->terminus) ? $train->terminus->name : "?";
+
+    my $dessertes;
+    if (defined $train->stations) {
+        my @arr_dessertes = map {
+            $_ ? ($_->name || $_->uic) : "Gare non trouvée"
+        } @{$train->stations};
+        $dessertes = join " \x{2022} ", @arr_dessertes;
+    }
+    else {
+        $dessertes = 'Desserte indisponible';
+    }
+
+    # HACK: les trains du RER D ont des numéros de la forme 123456-123457
+    # car ces trains changent de « parité » entre Châtelet–Les Halles et
+    # Paris Gare de Lyon.
+    return {
+        mission => $train->code,
+        numero  => substr($train->number, 0, 6),
+        time    => format_time_info($train),
+        destination => $terminus_name,
+        dessertes   => $dessertes,
+        platform => $train->platform,
+        trainclass => train_css_class($train),
+        retard => format_delay($train),
+        ligne => $train->line,
+    };
+}
 
 1;
 
