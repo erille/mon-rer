erille/mon-rer

commit 11d5bcc101858b339b4e35ac942f0f834e8e2f9a

Unify stations
The biggest and longest-lasting issue with this code was its assumption that
each station has one and only one three-letter code and one and only one UIC
code. This assumption quickly turned out to be false: for example, if a
station serves both RATP- and SNCF-owned lines, then that station has two
separate entities. Likewise, large Parisian stations like Gare de Lyon or Gare
du Nord are split into sub-entities (one for the main lines, one for RER lines
for example) which all have their own codes.

The solution is to reuse the pa_id from the SNCF’s internal station database
as a unifying identifier. This meant recreating my own station database from
scratch using the data that’s already there, only organized differently. Names
are directly given to pa_ids; a pa_id may correspond to one or more UIC
code or three-letter code; and a station’s list of served lines is assigned
through its pa_id. Let’s now hope that this identifier remains stable.

The code in its current state exhibited some weird behaviors that gave away
the problem: for example, a train serving Paris Gare de Lyon would have that
station three times in its stops list. This is no longer the case.

Because this assumption was quite widespread in the code, large portions
needed to be rewritten.

Fortunately for us, when a station has multiple UIC codes, Transilien’s live
train times API will return the correct data regardless of the actual UIC code
in the request.

public/js/autocomp.js
@@ -142,17 +142,15 @@ function autocomp_set(list) {
 
     for (var i = 0; i < list.length; i++) {
         var n = document.createElement("li");
-        var name = list[i].name;
-        var code = list[i].code;
 
         n.innerHTML = '<span class="name">'
-            + name
+            + list[i].name
             + '\u00a0<span class="lines">'
             + autocomp_line_images(list[i].lines)
             + '</span></span><span class="trig">'
-            + code
+            + list[i].codes.join(', ')
             + '</span>';
-        n.onclick = autocomp_make_click_handler(code, name);
+        n.onclick = autocomp_make_click_handler(list[i].codes[0], name);
         n.onmouseover = function(i) {
             return function() {
                 autocomp_set_highlight(i);

install/create-rer-web-tables.sql
@@ -19,15 +19,21 @@ CREATE TABLE IF NOT EXISTS metadata (
   value TEXT NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS gares (
-  code TEXT,
-  uic TEXT NOT NULL,
-  name TEXT NOT NULL COLLATE "fr_FR",
-  is_transilien BOOL NOT NULL DEFAULT TRUE);
+CREATE TABLE station_codes (
+  pa_id INTEGER NOT NULL,
+  code  TEXT NOT NULL,
+  uic   TEXT NOT NULL
+);
 
-CREATE TABLE IF NOT EXISTS gares_lines (
-  uic TEXT NOT NULL,
-  line TEXT NOT NULL);
+CREATE TABLE station_names (
+  pa_id INTEGER NOT NULL,
+  name  TEXT NOT NULL
+);
+
+CREATE TABLE station_lines (
+  pa_id INTEGER NOT NULL,
+  line TEXT NOT NULL
+);
 
 /*
  * La table de transcodage pour faire les conversions entre UIC7, UIC8 et ID

install/create-rer-web-views.sql
@@ -1,19 +1,36 @@
 /*
- * Cette vue permet de passer d’un code UIC à un stop_id du GTFS en utilisant
- * la table de transcodage fournie par la SNCF.
+ * Ces vues permettent de passer du « monde GTFS » à mes propres sources de
+ * données pour les noms, les trigrammes et les codes UIC des gares.
  */
 
-CREATE OR REPLACE VIEW uic_stop_id AS (
+/* Notre identifiant central est le pa_id. */
+
+CREATE OR REPLACE VIEW stop_id_pa_ids AS (
   WITH ids AS (
-    SELECT DISTINCT uic7,
+    SELECT DISTINCT pa_id,
                     'IDFM:monomodalStopPlace:' || zdlr_id AS stop_id_zdlr,
                     'IDFM:' || lda_id AS stop_id_lda
       FROM transco_icar
-     WHERE uic7 IS NOT NULL
-       AND zde_mode = 'TRAIN'
+     WHERE zde_mode = 'TRAIN'
   )
-  SELECT uic7, stop_id_zdlr AS stop_id
+  SELECT stop_id_zdlr AS stop_id, pa_id
     FROM ids
-   UNION SELECT uic7, stop_id_lda
+   UNION SELECT stop_id_lda, pa_id
            FROM ids
+);
+
+/* Cette vue fait l’association stop_id GTFS -> nom de gare. */
+
+CREATE OR REPLACE VIEW stop_id_station_names AS (
+  SELECT stop_id, name
+    FROM stop_id_pa_ids
+         JOIN station_names ON (stop_id_pa_ids.pa_id = station_names.pa_id)
+);
+
+/* Cette vue fait l’association stop_id GTFS vers codes trigramme et UIC */
+
+CREATE OR REPLACE VIEW stop_id_station_codes AS (
+  SELECT stop_id, stop_id_pa_ids.pa_id, code, uic
+    FROM stop_id_pa_ids
+         JOIN station_codes ON (stop_id_pa_ids.pa_id = station_codes.pa_id)
 );

install/deindex-rer-web-tables.sql
@@ -1,17 +1,12 @@
 ALTER TABLE metadata
   DROP CONSTRAINT metadata_pkey;
 
-DROP INDEX IF EXISTS "gares_idx_uic";
+ALTER TABLE station_codes
+  DROP CONSTRAINT station_codes_code_check,
+  DROP CONSTRAINT station_codes_uic_check;
 
-DROP INDEX IF EXISTS "gares_idx_name";
+ALTER TABLE station_names
+  DROP CONSTRAINT station_names_pkey;
 
-ALTER TABLE gares_lines
-  DROP CONSTRAINT IF EXISTS gares_lines_fk_gares,
-  DROP CONSTRAINT IF EXISTS gares_lines_pkey;
-
-ALTER TABLE gares
-  DROP CONSTRAINT IF EXISTS gares_code_check,
-  DROP CONSTRAINT IF EXISTS gares_uic_check,
-  DROP CONSTRAINT IF EXISTS gares_uic_unique,
-  DROP CONSTRAINT IF EXISTS gares_pkey;
-                                                        
+ALTER TABLE station_lines
+  DROP CONSTRAINT station_lines_pkey;

install/func-autocomplete-stations.sql
@@ -6,7 +6,7 @@
 DROP FUNCTION IF EXISTS autocomplete_stations;
 
 CREATE OR REPLACE FUNCTION autocomplete_stations(query TEXT)
-  RETURNS TABLE(code TEXT, name TEXT, uic TEXT, lines TEXT[],
+  RETURNS TABLE(codes TEXT[], name TEXT, lines TEXT[],
                 matched_name TEXT,
                 score FLOAT, score2 FLOAT)
   LANGUAGE SQL
@@ -19,19 +19,30 @@ WITH tsq(tsq_name, tsq_code) AS (
                       || ':*'),
          phraseto_tsquery('simple', query)
 ), results AS (
-  SELECT gares.code,
-         gares.name,
-         gares.uic, array_agg(line) AS lines,
+  SELECT station_codes_lines.codes,
+         station_names.name,
+         station_codes_lines.lines,
          ts_headline('fr_unaccent', name, tsq_name) AS matched_name,
          ts_rank_cd(to_tsvector('fr_unaccent', unaccent(name)), tsq_name) AS score,
-         ts_rank_cd(to_tsvector('simple', code), tsq_code) AS score2
-    FROM gares
+         ts_rank_cd(to_tsvector('simple', array_to_string(station_codes_lines.codes, ' ')), tsq_code) AS score2
+    FROM station_names
+         JOIN (SELECT station_codes_2.pa_id,
+                      station_codes_2.codes,
+                      array_agg(station_lines.line ORDER BY station_lines.line)
+                        AS lines
+                 FROM (SELECT pa_id, array_agg(code) AS codes
+                         FROM station_codes
+                        GROUP BY station_codes.pa_id)
+                        AS station_codes_2
+                      JOIN station_lines
+                          ON (station_codes_2.pa_id = station_lines.pa_id)
+                GROUP BY station_codes_2.pa_id, station_codes_2.codes)
+                AS station_codes_lines
+                ON (station_names.pa_id = station_codes_lines.pa_id)
          JOIN tsq ON (to_tsvector('fr_unaccent', unaccent(name)) @@ tsq.tsq_name
-                      OR to_tsvector('simple', code) @@ tsq.tsq_code)
-         JOIN gares_lines ON (gares.uic = gares_lines.uic)
-   GROUP BY gares.code, tsq_name, tsq_code
+                      OR to_tsvector('simple', array_to_string(station_codes_lines.codes, ' ')) @@ tsq.tsq_code)
 )
-  SELECT code, name, uic, lines, matched_name, 50*score2+score, 0
+  SELECT codes, name, lines, matched_name, 50*score2+score, 0
   FROM results
   ORDER BY 50*score2+score DESC
   LIMIT 10;

install/func-find-station-by-key.sql
@@ -1,0 +1,38 @@
+/* 
+ * Cette fonction recherche une gare par un code UIC ou un code TR3.
+ * Si key = 'uic', alors la recherche se fait par code UIC.
+ * Si key = 'code', alors la recherche se fait par code TR3.
+ */
+
+CREATE OR REPLACE FUNCTION find_station_by_key(key TEXT, value TEXT)
+  RETURNS TABLE (code TEXT, uic TEXT, name TEXT, lines TEXT[])
+  LANGUAGE SQL
+AS $$
+WITH stations_by_key AS (
+  SELECT pa_id,
+         'uic' AS key,
+         uic AS value
+    FROM station_codes
+   UNION ALL SELECT pa_id,
+                    'code' AS key,
+                    code AS value
+    FROM station_codes
+)
+SELECT station_codes.code,
+       station_codes.uic,
+       station_names.name,
+       array_agg(station_lines.line ORDER BY line) AS lines
+ FROM stations_by_key
+      JOIN station_codes
+     ON (CASE
+         WHEN stations_by_key.key = 'uic'
+           THEN stations_by_key.value = station_codes.uic
+         WHEN stations_by_key.key = 'code'
+           THEN stations_by_key.value = station_codes.code
+         END)
+      JOIN station_names ON (stations_by_key.pa_id = station_names.pa_id)
+      JOIN station_lines ON (stations_by_key.pa_id = station_lines.pa_id)
+  WHERE stations_by_key.key = find_station_by_key.key
+    AND stations_by_key.value = find_station_by_key.value
+  GROUP BY station_codes.code, station_codes.uic, station_names.name;
+$$

install/func-next-scheduled-trains.sql
@@ -52,9 +52,8 @@ WITH dates(date) AS (
                         AS times ON TRUE
          JOIN raw.routes ON (today_trips.route_id = routes.route_id)
          JOIN raw.stops ON (times.stop_id = stops.stop_id)
-         JOIN uic_stop_id ON (stops.stop_id = uic_stop_id.stop_id)
-         JOIN gares ON (uic_stop_id.uic7 = gares.uic)
-   WHERE gares.code = station_code
+         JOIN stop_id_station_codes ON (stops.stop_id = stop_id_station_codes.stop_id)
+   WHERE stop_id_station_codes.code = station_code
      AND rt <= today_trips.date + times.due_time
      AND today_trips.date + times.due_time <= rt + interval '6 hours'
 )
@@ -66,19 +65,19 @@ SELECT timetable.line,
        COALESCE(next_stops.destination, E'\x1F0\x1FTrain terminus') AS destination
   FROM timetable
        LEFT JOIN LATERAL (
-         SELECT DISTINCT array_agg(CONCAT(gares.code, E'\x1F', gares.uic, E'\x1F',
-                                         gares.name)) OVER w
-                             AS next_stops,
-                         last_value(CONCAT(gares.code, E'\x1F', gares.uic, E'\x1F',
-                                             gares.name)) OVER w
-                             AS destination
+         SELECT DISTINCT array_agg(E'\x1F0\x1F' || stop_id_station_names.name)
+                           OVER w
+                           AS next_stops,
+                         last_value(E'\x1F0\x1F' || stop_id_station_names.name)
+                           OVER w
+                           AS destination
            FROM raw.stop_times
-                LEFT JOIN uic_stop_id ON (stop_times.stop_id = uic_stop_id.stop_id)
-                JOIN gares ON (uic_stop_id.uic7 = gares.uic)
+                LEFT JOIN stop_id_station_names
+                    ON (stop_times.stop_id = stop_id_station_names.stop_id)
           WHERE stop_times.trip_id = timetable.trip_id
             AND stop_times.stop_sequence > timetable.stop_sequence
-         WINDOW w AS (ORDER BY stop_sequence
-                      ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING)
+                WINDOW w AS (ORDER BY stop_sequence
+                             ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING)
           LIMIT 1
        ) AS next_stops ON TRUE
   ORDER BY due_time

install/func-schedule-info-for-trains.sql
@@ -61,9 +61,8 @@ WITH dates(date) AS (
                         AS times ON TRUE
          JOIN raw.routes ON (today_trips.route_id = routes.route_id)
          JOIN raw.stops ON (times.stop_id = stops.stop_id)
-         JOIN uic_stop_id ON (stops.stop_id = uic_stop_id.stop_id)
-         JOIN gares ON (uic_stop_id.uic7 = gares.uic)
-   WHERE gares.code = station_code
+         JOIN stop_id_station_codes ON (stops.stop_id = stop_id_station_codes.stop_id)
+   WHERE stop_id_station_codes.code = station_code
      AND rt - interval '6 hours' <= today_trips.date + times.due_time
      AND today_trips.date + times.due_time <= rt + interval '6 hours'
 ), info AS (
@@ -77,15 +76,15 @@ WITH dates(date) AS (
            AS destination
     FROM timetable
          LEFT JOIN LATERAL (
-           SELECT DISTINCT array_agg(CONCAT(gares.code, E'\x1F', gares.uic, E'\x1F',
-                                            gares.name)) OVER w
+           SELECT DISTINCT array_agg(E'\x1F0\x1F' || stop_id_station_names.name)
+                             OVER w
                              AS next_stops,
-                           last_value(CONCAT(gares.code, E'\x1F', gares.uic, E'\x1F',
-                                             gares.name)) OVER w
+                           last_value(E'\x1F0\x1F' || stop_id_station_names.name)
+                             OVER w
                              AS destination
              FROM raw.stop_times
-                  LEFT JOIN uic_stop_id ON (stop_times.stop_id = uic_stop_id.stop_id)
-                  JOIN gares ON (uic_stop_id.uic7 = gares.uic)
+                  LEFT JOIN stop_id_station_names
+                      ON (stop_times.stop_id = stop_id_station_names.stop_id)
             WHERE stop_times.trip_id = timetable.trip_id
               AND stop_times.stop_sequence > timetable.stop_sequence
            WINDOW w AS (ORDER BY stop_sequence

commit 876af75199d689dbe0a6d1ea1c28173473ebb24b

Bugfix: changing stations was slightly broken
Fix a bug introduced in commit 11d5bcc, where selecting a station in the
autocompletion list would fail to replace the station name in the browser
title and the station selection control, instead replacing it with an empty
string.

public/js/autocomp.js
@@ -150,7 +150,7 @@ function autocomp_set(list) {
             + '</span></span><span class="trig">'
             + list[i].codes.join(', ')
             + '</span>';
-        n.onclick = autocomp_make_click_handler(list[i].codes[0], name);
+        n.onclick = autocomp_make_click_handler(list[i].codes[0], list[i].name);
         n.onmouseover = function(i) {
             return function() {
                 autocomp_set_highlight(i);