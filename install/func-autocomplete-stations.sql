a858e12ed4e0917b84e27f0b440071983a8555fa
x0r <x0r+bitbucket@x0r.fr>
2022-03-28 18:37:45 +0200

    Improve suggested stations when autocompleting
    Ever since migrating from MySQL to PostgreSQL, I wanted to try using
    PostgreSQL's more advanced full-text search (FTS) capabilities for station
    name completion.
    
    However, the ranking of the suggestions was still unsatisfactory. That is
    because PostgreSQL’s built-in functions for ranking work best in a slightly
    different use case than ours, i.e. searching text inside larger documents,
    whereas here, they are used for suggesting names. After many hours of
    experimentation, I came to the conclusion that a good ranking algorithm would
    rank the results in the following order: complete matches first, then prefix
    matches, then matches starting anywhere. The new rewritten query does exactly
    that.
    
    Searches are performed on the station's code and name, which means that a
    string of "cl" will yield Creil as its first result because its code is "CL";
    then all stations whose name or code start with "cl" follow; then all stations
    which contain a word starting with "cl" anywhere but at the beginning are
    last.
    
    Station names also need to be cleaned up a bit, because hyphens in names
    should be treated as word breaks and the simple text search configuration does
    not yield satisfactory results in this case. Writing a preprocessing function
    in SQL and running queries and stations names through it before converting
    those strings to tsvectors or tsqueries is crude but works as it should.
    
    So with all this work, station searching should be much closer to what I
    believe is correct. One small possible improvement would be to use historical
    ridership data, or some other kind of “popularity contest”-type of ranking, as
    an extra input to the ranking algorithm in order to break ties.

--- install/func-autocomplete-stations.sql
+++ install/func-autocomplete-stations.sql
@@ -1,16 +1,40 @@
 /*
- * Cette fonction renvoie une liste de suggestions d’autocomplétions pour une
- * recherche partielle de nom de gare.
+ * Passe la chaîne de caractères en minuscules et remplace toute suite d'au
+ * moins un caractère non alphanumérique par un espace.
  */
 
-DROP FUNCTION IF EXISTS autocomplete_stations;
+CREATE OR REPLACE FUNCTION simplify_phrase(text TEXT)
+  RETURNS TEXT
+  LANGUAGE SQL
+AS $$
+  SELECT regexp_replace(lower(unaccent(text)), '\W+', ' ', 'g');
+$$
+IMMUTABLE;
+
+/*
+ * Crée un tsquery permettant de rechercher une suite exacte de mots.
+ */
+
+CREATE OR REPLACE FUNCTION unaccent_phraseto_tsquery(query TEXT)
+  RETURNS tsquery
+  LANGUAGE SQL
+AS $$
+  SELECT phraseto_tsquery('fr_unaccent', simplify_phrase(query));
+$$
+IMMUTABLE;
+
+/*
+ * Crée un tsquery permettant de rechercher une suite de mots, dont le
+ * dernier est un préfixe (par exemple, pour trouver "Saint-Denis" en
+ * tapant juste "saint-de").
+ */
 
 CREATE OR REPLACE FUNCTION prefix_phraseto_tsquery(query TEXT)
   RETURNS tsquery
   LANGUAGE SQL
 AS $$
   WITH tsq(q) AS (
-    SELECT NULLIF(phraseto_tsquery('fr_unaccent', query)::text, '')
+    SELECT NULLIF(unaccent_phraseto_tsquery(query)::text, '')
   )
   SELECT CASE
          WHEN tsq.q IS NULL THEN NULL
@@ -20,42 +44,107 @@ AS $$
 $$
 IMMUTABLE;
 
-CREATE OR REPLACE FUNCTION autocomplete_stations(query TEXT)
-  RETURNS TABLE(codes TEXT[], name TEXT, lines TEXT[],
-                matched_name TEXT,
-                score FLOAT, score2 FLOAT)
+/*
+ * Crée un tsvector en supprimant les accents et les autres caractères non
+ * alphanumériques, facilitant la recherche et l'indexation.
+ */
+
+CREATE OR REPLACE FUNCTION unaccent_to_tsvector(config regconfig, text TEXT)
+  RETURNS tsvector
   LANGUAGE SQL
 AS $$
-WITH tsq(tsq_name, tsq_code) AS (
-  SELECT prefix_phraseto_tsquery(query),
-         phraseto_tsquery('simple', query)
-), results AS (
-  SELECT station_codes_lines.codes,
-         station_names.name,
-         station_codes_lines.lines,
-         ts_headline('fr_unaccent', name, tsq_name) AS matched_name,
-         ts_rank_cd(to_tsvector('fr_unaccent', name), tsq_name) AS score,
-         ts_rank_cd(to_tsvector('simple', array_to_string(station_codes_lines.codes, ' ')), tsq_code) AS score2
-    FROM station_names
-         JOIN (SELECT station_codes_2.pa_id,
-                      station_codes_2.codes,
-                      array_agg(station_lines.line ORDER BY station_lines.line)
-                        AS lines
-                 FROM (SELECT pa_id, array_agg(code) AS codes
-                         FROM station_codes
-                        GROUP BY station_codes.pa_id)
-                        AS station_codes_2
-                      JOIN station_lines
-                          ON (station_codes_2.pa_id = station_lines.pa_id)
-                GROUP BY station_codes_2.pa_id, station_codes_2.codes)
-                AS station_codes_lines
-                ON (station_names.pa_id = station_codes_lines.pa_id)
-         JOIN tsq ON (to_tsvector('fr_unaccent', name) @@ tsq.tsq_name
-                      OR to_tsvector('simple', array_to_string(station_codes_lines.codes, ' ')) @@ tsq.tsq_code)
-)
-  SELECT codes, name, lines, matched_name, 50*score2+score, 0
-  FROM results
-  ORDER BY 50*score2+score DESC
-  LIMIT 10;
+  SELECT to_tsvector(config, simplify_phrase(text));
 $$
+IMMUTABLE;
+
+/*
+ * Cette fonction renvoie une liste de suggestions d’autocomplétions pour une
+ * recherche partielle de nom ou de code de gare.
+ *
+ * Les résultats sont, par ordre de préférence décroissante :
+ *
+ * (a) les correspondances exactes sur le code ou le nom (par exemple, "CL" va
+ *     faire monter "Creil" en tout premier car son code est "CL") ;
+ * (b) les correspondances partielles au début du code ou du nom (par exemple,
+ *     toutes les gares dont le code ou le nom commence par "cl") ;
+ * (c) les correspondances survenant partout ailleurs dans le nom en début de
+ *     mot (par exemple, toutes les gares dont le nom comporte un mot
+ *     commençant par "cl").
+ */
+
+DROP FUNCTION IF EXISTS autocomplete_stations;
+
+CREATE OR REPLACE FUNCTION autocomplete_stations(query TEXT)
+  RETURNS TABLE(codes TEXT[], name TEXT, lines TEXT[], score FLOAT)
+  LANGUAGE SQL
+  AS $$
+WITH tsq AS (
+  SELECT '''<begin>''' <-> unaccent_phraseto_tsquery(query) <-> '''<end>'''
+           AS exact,
+         '''<begin>''' <-> prefix_phraseto_tsquery(query)
+           AS at_start,
+         prefix_phraseto_tsquery(query) AS anywhere
+), code_matches AS (
+  SELECT pa_id,
+         MAX(ts_rank_cd(code_tsv, tsq.exact)) AS score_exact,
+         MAX(ts_rank_cd(code_tsv, tsq.at_start)) AS score_at_start
+    FROM (SELECT pa_id,
+                 setweight('<begin>:1'::tsvector
+                               || to_tsvector('simple', code)
+                               || '<end>:1'::tsvector,
+                           'A')
+                   AS code_tsv
+            FROM station_codes) AS station_codes_tsq
+         JOIN tsq ON (code_tsv @@ tsq.anywhere)
+   GROUP BY pa_id
+), name_matches AS (
+  SELECT pa_id,
+         ts_rank_cd(name_tsv, tsq.exact) AS score_exact,
+         ts_rank_cd(name_tsv, tsq.at_start) AS score_at_start,
+         ts_rank_cd(name_tsv, tsq.anywhere) AS score_anywhere
+    FROM (SELECT pa_id,
+                 '<begin>:1'::tsvector
+                   || unaccent_to_tsvector('fr_unaccent', name)
+                   || '<end>:1'::tsvector
+                   AS name_tsv
+            FROM station_names) AS station_names_tsv
+         JOIN tsq ON (name_tsv @@ tsq.anywhere)
+), ranked_matches AS (
+  SELECT pa_id,
+         SUM(score_exact) AS score_exact,
+         SUM(score_at_start) AS score_at_start,
+         SUM(score_anywhere) AS score_anywhere
+    FROM (
+      SELECT pa_id, score_exact, score_at_start, 0 AS score_anywhere
+        FROM code_matches
+       UNION ALL
+      SELECT pa_id, score_exact, score_at_start, score_anywhere
+        FROM name_matches)
+           AS scores
+   GROUP BY pa_id
+)
+SELECT codes,
+       name,
+       lines,
+       100 * score_exact + 10 * score_at_start + score_anywhere AS score
+  FROM ranked_matches
+       JOIN (
+         SELECT pa_id, array_agg(code) AS codes
+           FROM station_codes
+          GROUP BY pa_id)
+              AS station_codes
+           ON (ranked_matches.pa_id = station_codes.pa_id)
+       JOIN (
+         SELECT pa_id, array_agg(line ORDER BY line) AS lines
+           FROM station_lines
+          GROUP BY pa_id)
+              AS station_lines
+           ON (ranked_matches.pa_id = station_lines.pa_id)
+       JOIN station_names ON (ranked_matches.pa_id = station_names.pa_id)
+ ORDER BY score_exact DESC,
+          score_at_start DESC,
+          score_anywhere DESC
+ LIMIT 10;
+$$
+ROWS 10
 STABLE;
