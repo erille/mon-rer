a3325fc02f5cfb040be42df1be5b2920f343aada
x0r <x0r+bitbucket@x0r.fr>
2024-05-17 15:50:12 +0200

    Add full support for Nanterre La Folie
    There was some missing info from SNCF that prevented us from supporting the
    newly-opened station at opening time. But now, it's fixed.
    
    The transcodification.csv file was downloaded off SNCF's website, but had to
    be completed with information on PRIM because some required columns were
    missing data.

--- install/import-rer-web-data.sql
+++ install/import-rer-web-data.sql
@@ -1,4 +1,4 @@
-\copy transco_icar from 'install/transco_cbx_lh_icar_V8.csv' (format csv, header)
+\copy transco_icar from 'install/transcodification.csv' (format csv, header, delimiter ';')
 \copy station_codes from 'install/station_codes.csv' (format csv, header)
 \copy station_names from 'install/station_names.csv' (format csv, header)
 \copy station_lines from 'install/station_lines.csv' (format csv, header)
