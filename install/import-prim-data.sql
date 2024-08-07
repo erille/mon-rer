d4c96da12c566995362225d399820bafbb3fd022
x0r <x0r+bitbucket@x0r.fr>
2024-08-07 09:29:32 +0200

    PRIM ZDA dataset: Change column order
    Upstream changed the order of columns in their CSV. Fortunately we can get
    around that without having to alter the schema of our own database.

--- install/import-prim-data.sql
+++ install/import-prim-data.sql
@@ -1,3 +1,3 @@
 \copy prim_relations from 'input/prim/relations.csv' (format csv, header, delimiter ';')
 
-\copy prim_zda from 'input/prim/zda.csv' (format csv, header, delimiter ';')
+\copy prim_zda ("ZdAId", "ZdAVersion", "ZdACreated", "ZdAChanged", "ZdAName", "ZdAXEpsg2154", "ZdAYEpsg2154", "ZdCId", "ZdAPostalRegion", "ZdATown", "ZdAType") from 'input/prim/zda.csv' (format csv, header, delimiter ';')
