269895f2413f99e6efb97de7d8658bae5aad27dc
Emmanuel Rille <erille@keplercheuvreux.com>
2026-04-15 16:14:16 +0200

    Fix standard template asset compatibility

--- app/main.py
+++ app/main.py
@@ -35,6 +35,7 @@ idfm_client = IdfmApiClient(settings.idfm_api_token, settings.request_timeout_se
 schedule_index = ScheduleIndex()
 departure_service = DepartureService(settings, stations, idfm_client, schedule_index)
 templates = Jinja2Templates(directory="app/templates")
+templates.env.globals["asset_version"] = "20260415-stdfix-1"
 DEFAULT_THEME = "moderne"
 SUPPORTED_THEMES = {DEFAULT_THEME, "standard"}
 
